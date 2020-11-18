5b6411c888e5cc688af20d7ad2f2497390e5d45c
YU <villayuts@edu.hku.hk>
2020-11-18 13:34:09 +0800

    ldsk12_beta v2.0.3
    - fix stem practice box shaking problem

--- config/app.php
+++ config/app.php
@@ -56,7 +56,7 @@ return [
 
     'asset_url' => env('ASSET_URL', null),
 
-    'app_ver' => '2.0.2',
+    'app_ver' => '2.0.3',
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
